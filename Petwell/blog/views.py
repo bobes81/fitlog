c30b4b0e76ab52d6dd7d73b33652f94d1bcc4513
Ivo Kimpl <ivokimpl@Mac.powerhub>
2025-10-23 11:36:46 +0100

    Add CRUD functionality for BlogPost model

--- Petwell/blog/views.py
+++ Petwell/blog/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import BlogPost
+from .forms import BlogPostForm
 
-# Create your views here.
+def blog_list(request):
+    posts = BlogPost.objects.all().order_by('-created_at')
+    return render(request, 'blog/blog_list.html', {'posts': posts})
+
+def blog_create(request):
+    if request.method == 'POST':
+        form = BlogPostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('blog_list')
+    else:
+        form = BlogPostForm()
+    return render(request, 'blog/blog_form.html', {'form': form})
+
+def blog_update(request, pk):
+    post = get_object_or_404(BlogPost, pk=pk)
+    if request.method == 'POST':
+        form = BlogPostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('blog_list')
+    else:
+        form = BlogPostForm(instance=post)
+    return render(request, 'blog/blog_form.html', {'form': form})
+
+def blog_delete(request, pk):
+    post = get_object_or_404(BlogPost, pk=pk)
+    if request.method == 'POST':
+        post.delete()
+        return redirect('blog_list')
+    return render(request, 'blog/blog_confirm_delete.html', {'post': post})
