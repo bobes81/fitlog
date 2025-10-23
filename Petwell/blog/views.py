523ab3ad22139a5910ea10fc8a2de748de24963d
Ivo Kimpl <ivokimpl@Mac.powerhub>
2025-10-23 11:45:54 +0100

    Protect blog CRUD views with login_required decorator

--- Petwell/blog/views.py
+++ Petwell/blog/views.py
@@ -1,34 +1,39 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 from .models import BlogPost
 from .forms import BlogPostForm
+
 
 def blog_list(request):
     posts = BlogPost.objects.all().order_by('-created_at')
     return render(request, 'blog/blog_list.html', {'posts': posts})
 
+
+@login_required
 def blog_create(request):
     if request.method == 'POST':
         form = BlogPostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
-            post.author = request.user
+            post.author = request.user  # uloží aktuálního přihlášeného uživatele
             post.save()
             return redirect('blog_list')
     else:
         form = BlogPostForm()
     return render(request, 'blog/blog_form.html', {'form': form})
 
+
+@login_required
 def blog_update(request, pk):
     post = get_object_or_404(BlogPost, pk=pk)
-    if request.method == 'POST':
-        form = BlogPostForm(request.POST, instance=post)
-        if form.is_valid():
-            form.save()
-            return redirect('blog_list')
-    else:
-        form = BlogPostForm(instance=post)
+    form = BlogPostForm(request.POST or None, instance=post)
+    if form.is_valid():
+        form.save()
+        return redirect('blog_list')
     return render(request, 'blog/blog_form.html', {'form': form})
 
+
+@login_required
 def blog_delete(request, pk):
     post = get_object_or_404(BlogPost, pk=pk)
     if request.method == 'POST':
